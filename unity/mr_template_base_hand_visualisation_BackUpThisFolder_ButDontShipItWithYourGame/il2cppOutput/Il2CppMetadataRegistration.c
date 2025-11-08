371da7849e6e829067bf27e1a47e216e64eeb27f
Madhav Lodha <madhavlodha2503@gmail.com>
2025-11-08 11:09:49 -0500

    feat: initialised the initial repository along with a new ARena scene, to be used as our primary scene. Added some extra packages along with some critical Quest Link infrastructure.

---
+++ unity/mr_template_base_hand_visualisation_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/Il2CppMetadataRegistration.c
@@ -0,0 +1,36 @@
+﻿#include "pch-c.h"
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+extern Il2CppGenericClass* const g_Il2CppGenericTypes[];
+extern const Il2CppGenericInst* const g_Il2CppGenericInstTable[];
+extern const Il2CppGenericMethodFunctionsDefinitions g_Il2CppGenericMethodFunctions[];
+extern const Il2CppType* const  g_Il2CppTypeTable[];
+extern const Il2CppMethodSpec g_Il2CppMethodSpecTable[];
+extern const int32_t* g_FieldOffsetTable[];
+extern const Il2CppTypeDefinitionSizes* g_Il2CppTypeDefinitionSizesTable[];
+IL2CPP_EXTERN_C const Il2CppMetadataRegistration g_MetadataRegistration;
+const Il2CppMetadataRegistration g_MetadataRegistration = 
+{
+	15259,
+	g_Il2CppGenericTypes,
+	10945,
+	g_Il2CppGenericInstTable,
+	150892,
+	g_Il2CppGenericMethodFunctions,
+	46740,
+	g_Il2CppTypeTable,
+	172657,
+	g_Il2CppMethodSpecTable,
+	13341,
+	g_FieldOffsetTable,
+	13341,
+	g_Il2CppTypeDefinitionSizesTable,
+	0,
+	NULL,
+};
